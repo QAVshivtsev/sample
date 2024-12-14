83bfd5c1dde7c7206a0bdcd87bafe0359ba005fe
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-12-14 15:15:45 +0300

    Готовое ДЗ

---
+++ app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
@@ -0,0 +1,86 @@
+package ru.netology.testing.uiautomator
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+
+const val MODEL_PACKAGE = "ru.netology.testing.uiautomator"
+
+const val TIMEOUT = 5000L
+
+@RunWith(AndroidJUnit4::class)
+class ChangeTextTest {
+
+    private lateinit var device: UiDevice
+    private val textToSet = "Netology"
+    private val badTextToSet = "     "
+    private val textToActivity = "Test Activity"
+
+    @Before
+    fun beforeEachTest() {
+        // Press home
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device.pressHome()
+
+        // Wait for launcher
+        val launcherPackage = device.launcherPackageName
+        device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
+    }
+
+    private fun waitForPackage(packageName: String) {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
+        context.startActivity(intent)
+        device.wait(Until.hasObject(By.pkg(packageName)), TIMEOUT)
+    }
+
+    @Test
+    fun testChangeGoodText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        device.findObject(By.res(packageName, "userInput")).text = textToSet
+        device.findObject(By.res(packageName, "buttonChange")).click()
+
+        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
+        assertEquals(result, textToSet)
+    }
+
+    @Test
+    fun testChangeBadText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        device.findObject(By.res(packageName, "userInput")).text = badTextToSet
+        device.findObject(By.res(packageName, "buttonChange")).click()
+
+        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
+        assertEquals(result,   "Hello UiAutomator!")
+    }
+
+    @Test
+    fun testInTheNewActivity() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        device.findObject(By.res(packageName, "userInput")).text = textToActivity
+        device.findObject(By.res(packageName, "buttonActivity")).click()
+        waitForPackage(packageName)
+
+        val result = device.findObject(By.res(packageName, "text")).text
+        assertEquals(result, textToActivity)
+    }
+}
+
+
+
